9a26510cc9a7f60719741a900e2a5818dcb72d42
bigmasonwang <bigmasonwang@gmail.com>
2021-02-09 22:07:16 +1030

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Button } from 'antd';
+import './App.css';
+import Footer from './components/Footer';
 
 const App = () => {
-  return <div className='App'>app</div>;
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+
+const Footer = () => {
+  return (
+    <>
+      <Row>
+        <Col span={6}>col-6</Col>
+        <Col span={6}>Our Menu</Col>
+        <Col span={6}>col-6</Col>
+        <Col span={6}>col-6</Col>
+      </Row>
+    </>
+  );
+};
+
+export default Footer;
